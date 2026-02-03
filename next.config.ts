009e4b88368198a93c82f0a78cfae34a45a78900
Mohamad Anas <anas@ryzer.app>
2026-02-03 15:48:41 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
-    domains: ["ryzer-v2.s3.ap-south-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ryzer-v2.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
 export default nextConfig;
-
